4b3ee727064eff9c2b3bf4da2b2072be34e738b5
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2020-04-03 08:50:41 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+#program to check palindrome string or not
+
+
+#input and initialization
+str1 = input()
+
+#reverse a string and initialsation
+str2 = str1[::-1]
+
+if(str1 == str2):
+    print('it is a palindrome')
+
+else:
+    print('it is not a palindrome')
+
+    
